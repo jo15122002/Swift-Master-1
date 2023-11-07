3081f237b97fd1f18c7b3f5edfddc0bab92503f4
digital <digital@iMac24-4.local>
2023-11-07 11:43:31 +0100

    creation du projet coreML

---
+++ SwiftUI_CoreML/SwiftUI_CoreML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_CoreML
+//
+//  Created by digital on 07/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_CoreML/SwiftUI_CoreML/SwiftUI_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoreMLApp.swift
+//  SwiftUI_CoreML
+//
+//  Created by digital on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
