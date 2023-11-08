e08949781ba30065f8794442ec0ace95af1dee59
digital <digital@iMac24-10.local>
2023-11-08 09:01:30 +0100

    Création du projet bluetooth

---
+++ SwiftUI_BLE/SwiftUI_BLE/SwiftUI_BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BLEApp.swift
+//  SwiftUI_BLE
+//
+//  Created by digital on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
