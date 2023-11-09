e429a026cbdee48fac74efaca72a87e43fef596e
digital <digital@iMac24-89.local>
2023-11-09 10:30:48 +0100

    création d'un projet de websocket avec envoi de data

--- SwiftUI_WS/SwiftUI_WS/ContentView.swift
+++ SwiftUI_WS/SwiftUI_WS/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message:String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Message to send", text: $message)
+                .onSubmit {
+                    WebSocketManager.instance.connectToUrl(url: "ws://172.28.55.240:8080/dmii"){
+                        print(message)
+                        WebSocketManager.instance.sendMessage(message: message){
+                            WebSocketManager.instance.disconnect()
+                        }
+                    }
+                }
         }
         .padding()
     }
--- SwiftUI_WS/SwiftUI_WS/WebSocketManager.swift
+++ SwiftUI_WS/SwiftUI_WS/WebSocketManager.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+import NWWebSocket
+import Network
+
+class WebSocketManager : WebSocketConnectionDelegate{
+    func webSocketDidConnect(connection: WebSocketConnection) {
+        print("connected")
+        self.connectedCallback()
+    }
+    
+    func webSocketDidDisconnect(connection: WebSocketConnection, closeCode: NWProtocolWebSocket.CloseCode, reason: Data?) {
+        print("disconnected")
+        self.disconnectedCallback()
+    }
+    
+    func webSocketViabilityDidChange(connection: WebSocketConnection, isViable: Bool) {
+        print("viability changed")
+    }
+    
+    func webSocketDidAttemptBetterPathMigration(result: Result<WebSocketConnection, NWError>) {
+        print("attempt better path migration")
+    }
+    
+    func webSocketDidReceiveError(connection: WebSocketConnection, error: NWError) {
+        print("error received : " + error.debugDescription)
+    }
+    
+    func webSocketDidReceivePong(connection: WebSocketConnection) {
+        print("pong")
+    }
+    
+    func webSocketDidReceiveMessage(connection: WebSocketConnection, string: String) {
+        print("message: " + string)
+    }
+    
+    func webSocketDidReceiveMessage(connection: WebSocketConnection, data: Data) {
+        print("received data message")
+    }
+    
+    
+    static let instance = WebSocketManager()
+    
+    private var socket:NWWebSocket?
+    
+    private var connectedCallback:()->() = {}
+    private var disconnectedCallback:()->() = {}
+    
+    func connectToUrl(url:String, intervalPing:Bool = false, callback:@escaping (()->()) = {}){
+        if let socketURL = URL(string: url){
+            self.socket = NWWebSocket(url: socketURL)
+            self.socket?.delegate = self
+            self.socket?.connect()
+            
+            self.connectedCallback = callback
+            
+            if(intervalPing){
+                socket?.ping(interval: 30.0)
+            }
+        }
+    }
+    
+    func disconnect(callback:@escaping (()->()) = {}){
+        self.disconnectedCallback = callback
+        self.socket?.disconnect()
+    }
+    
+    func sendMessage(message:String, callback:@escaping (()->()) = {}){
+        socket?.send(string: message)
+        callback()
+    }
+    
+    func sendData(data:Data, callback:@escaping (()->()) = {}){
+        self.socket?.send(data: data)
+        callback()
+    }
+    
+}
