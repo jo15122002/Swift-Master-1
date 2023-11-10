b1d790ae4bec463407b31389d8eaa022b39e3324
digital <digital@iMac24-89.local>
2023-11-10 09:09:46 +0100

    création du projet bestfriend

---
+++ SwiftUI_BestFriend/SwiftUI_BestFriend/SwiftUI_BestFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BestFriendApp.swift
+//  SwiftUI_BestFriend
+//
+//  Created by digital on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BestFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
