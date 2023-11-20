61cb09f284ebfde9dc690515671d2b9f4cd5772b
digital <digital@iMac24-77.local>
2023-11-20 08:47:47 +0100

    création projet storyboard

---
+++ Swift_StoryBoard/Swift_StoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_StoryBoard
+//
+//  Created by digital on 20/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
