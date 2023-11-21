4a11ff49e498676e897bc843961c3d3ebc10d9e5
digital <digital@iMac24-166.local>
2023-11-21 09:31:03 +0100

    Création du tp final

---
+++ Tp_Final/Tp_Final/Tp_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tp_FinalApp.swift
+//  Tp_Final
+//
+//  Created by digital on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tp_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
